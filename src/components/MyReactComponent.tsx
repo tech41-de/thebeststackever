38bdd9e3a15e89e0147e023459583a244590ed88
Mathias Dietrich <mathias.dietrich@tech41.de>
2024-10-01 16:08:31 +0200

    rbots.txt added

--- src/components/MyReactComponent.tsx
+++ src/components/MyReactComponent.tsx
@@ -26,25 +26,20 @@ export default function MyReactComponent() {
         </div>
         <Text size="7xl">The best JAMstack ever</Text>
         <Text size="xl">
-           <a href="https://www.cloudflare.com/">Cloudflare Pages</a> - 
-           <a href="https://nodejs.org/">Node</a> - 
-           <a href="https://astro.build/">Asto</a> - 
-           <a href="https://react.dev/">React</a> - 
-           <a href="https://panda-css.com/">Panda CSS</a> - 
-           <a href="https://ark-ui.com/">ARK UI</a> - 
-           <a href="https://park-ui.com/">Park UI</a> - 
+           <a href="https://www.cloudflare.com/">Cloudflare Pages</a> •{' '}
+           <a href="https://nodejs.org/">Node</a> • {' '}
+           <a href="https://astro.build/">Asto</a> • {' '}
+           <a href="https://react.dev/">React</a> • {' '}
+           <a href="https://panda-css.com/">Panda CSS</a> • {' '}
+           <a href="https://ark-ui.com/">ARK UI</a> • {' '}
+           <a href="https://park-ui.com/">Park UI</a> • {' '}
            <a href="https://lucide.dev/">Lucide</a>
              </Text>
         <br />
         <a href="https://github.com/tech41-de/thebeststackever">
-        
         {!dark &&<img src="github.svg"  width={75} height={75}/>}
         {dark &&<img src="github_white.svg"  width={75} height={75}/>}
-
         </a>
-        <br />
-        <br />
-       
         <img src="/android-chrome-512x512.png" width={450} height={450}/>
         <Avatar name="Mathias Dietrich" src="/mat.png" /><Text size="sm">TECH41 GmbH <a href="https://tech41.de">tech41.de</a></Text>
       </center>
